78ad5e6e6e9f444f5cad68ca2dd3d940503e85b1
ansen <anhui@365yf.com>
2016-11-18 15:52:43 +0800

    第一次提交项目

---
+++ app/src/main/java/com/ansen/circleimageview/app/MainActivity.java
@@ -0,0 +1,24 @@
+package com.ansen.circleimageview.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.ansen.circleimageview.CircleImageView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CircleImageView ciImage2= (CircleImageView) findViewById(R.id.ci_image2);
+        ciImage2.setBorder(getResources().getColor(android.R.color.holo_red_dark),2);//设置边框
+
+
+    }
+
+
+}
